899397d54b5f67a9b8f100888820ac358b501cad
Mridul1703 <mridulvasudeva.03@gmail.com>
2023-04-22 17:55:32 +0530

    app.js changes

--- sportzy/src/App.js
+++ sportzy/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Faq from './Components/Faq';
+import Footer from './Components/Footer';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Navbar />
+       <Router>
+      <Switch>
+        <Route exact path='/'>
+            <Navbar />
+            <Hero />
+            <Faq />
+            {/* <GroundListing /> */}
+            <Footer />  
+          </Route>
+      </Switch>
+    </Router>
     </>
   );
 }
