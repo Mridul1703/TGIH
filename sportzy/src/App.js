d8a42ae2b6abfe6649859118e0430ad834e426c2
Mridul1703 <mridulvasudeva.03@gmail.com>
2023-04-22 15:32:11 +0530

    ground listing + Routing

--- sportzy/src/App.js
+++ sportzy/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
-import Footer from './Components/Footer';
 import Faq from './Components/Faq';
+import GroundListing from './Components/GroundListing';
+import Footer from './Components/Footer';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero></Hero>
-      <Faq></Faq>
-      <Footer></Footer>
+    <Router>
+      <Routes>
+        <Route>
+          <Navbar />
+          <Hero />
+          <Faq />
+          <GroundListing />
+          <Footer />
+        </Route>
+      </Routes>
+    </Router>
     </>
   );
 }
